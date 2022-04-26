0f659f9b1df850cad885daab02440dd4a2de8470
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-04-26 15:34:48 +0300

    movie Class

--- app/config.py
+++ app/config.py
@@ -2,4 +2,4 @@ class Config:
     '''
     General configuration parent class
     '''
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/550?api_key=16959023f18ddc2876352ff50130250c'
